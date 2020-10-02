b0ec8f9e8fe6bd6dff804e13cbcaba2be5d1de15
hacklover <info@hacklover.net>
2020-10-02 13:25:49 +0200

    Minor improvements

--- client/windows/WindowSnake.vue
+++ client/windows/WindowSnake.vue
@@ -68,7 +68,7 @@
 </template>
 
 <script>
-  import Window from "../../../core/components/window/Window";
+  import Window from "~/core/components/window/Window";
   import p5 from 'p5';
 
   export default {
